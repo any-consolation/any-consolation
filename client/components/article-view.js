f93fecc70dddf8e6861b925e0f7f5eeb1e39c084
mikekanter6@gmail.com <29645046+mikekanter@users.noreply.github.com>
2018-04-23 11:20:57 -0500

    article view and store

---
+++ client/components/article-view.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getArticleThunk } from '../store'
+// import history
+
+class ArticleView extends Component {
+  constructor() {
+    super()
+    // console.log(this.props.history.location.pathname)
+  }
+
+  componentDidMount() {
+    this.props.getArticle(this.props.history.location.pathname)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>hello there</h1>
+        <h4>{this.props.article && this.props.article.title}</h4>
+        <p>{this.props.article && this.props.article.content}</p>
+      </div>
+    )
+  }
+}
+
+const mapState = state => {
+  return {
+    article: state.article
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    getArticle(url) {
+      dispatch(getArticleThunk(url))
+    }
+  }
+}
+
+export default connect(mapState, mapDispatch)(ArticleView)
