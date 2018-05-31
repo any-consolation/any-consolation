ccdcf0857fad81695b6e1ed943711a6869f49d56
mikekanter6@gmail.com <29645046+mikekanter@users.noreply.github.com>
2018-05-31 15:18:25 -0500

    no unverify button for admin users

--- client/routes.js
+++ client/routes.js
@@ -14,7 +14,7 @@ class Routes extends Component {
   }
 
   render() {
-    const { isLoggedIn } = this.props
+    const { isLoggedIn, isAdmin } = this.props
 
     return (
       <Switch>
@@ -28,7 +28,7 @@ class Routes extends Component {
           isLoggedIn &&
           <Switch>
             {/* Routes placed here are only available after logging in */}
-            <Route path="/home" component={UserHome} />
+            <Route path="/userHome" component={UserHome} />
           </Switch>
         }
         {/* Displays our Login component as a fallback */}
@@ -45,7 +45,8 @@ const mapState = (state) => {
   return {
     // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
     // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.id
+    isLoggedIn: !!state.user.id,
+    isAdmin: !!state.user.isAdmin
   }
 }
 
