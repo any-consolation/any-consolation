da71acded37a9ca00efe37a0c3344e49df0718a9
mikekanter6@gmail.com <29645046+mikekanter@users.noreply.github.com>
2018-06-14 08:53:44 -0500

    starting author portal

---
+++ client/components/author-portal.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class AuthorPortal extends Component {
+
+  constructor() {
+    super()
+  }
+
+
+  render() {
+
+    return (
+      <div>
+        {/* list of author's articles */}
+        {/* edit articles, change to anon, unpublish */}
+        {/* submit new article form */}
+      </div>
+    )
+
+  }
+
+}
